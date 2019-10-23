5d536eeb90cead68849e4a420e7830a3cc4a6a85
Peter Milovcik <peter.milovcik@gmail.com>
2019-10-23 14:32:29 +0200

    Initial commit

---
+++ Open-Lab-02.02/Formatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_02._02
+{
+    public class Formatter
+    {
+        public void Format(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-02.02/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Open_Lab_02._02
+{
+    [TestFixture]
+    public class Tests
+    {
+        private TextWriter consoleOutput;
+        private StringWriter stringWriter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            consoleOutput = Console.Out;
+            stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            stringWriter.Dispose();
+            Console.SetOut(consoleOutput);
+        }
+
+        [Test]
+        public void Format()
+        {
+            var formatter = new Formatter();
+            var numbers = new[] {1.23456, 2.34567, 3.45678, 4.56789, 5.67890};
+            formatter.Format(numbers);
+            stringWriter.Flush();
+            Assert.That(stringWriter.ToString(), Is.EqualTo("1.23 2.35 3.46 4.57 5.68"));
+        }
+    }
+}
