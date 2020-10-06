790accce2cb510dac2eb9525a80296c8e8bbe571
Viktor <krpectv57@gmail.com>
2020-10-06 12:42:02 +0200

    -

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+          string vratit = null;
+          for (int i = 0; i < 5; i++)
+          {
+                numbers[i] = Math.Round(numbers[i], 2,MidpointRounding.AwayFromZero);
+                vratit = vratit + numbers[i].ToString() + "";
+          }
+            Console.Write(vratit.Replace(",", "."));
         }
     }
 }
